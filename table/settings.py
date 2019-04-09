45fbbf2d560caefc7a8175f05f977edf72c0a8ab
StarFox <adlin.vladislav@gmail.com>
2019-04-09 13:51:21 +0300

    Update settings.py

--- table/settings.py
+++ table/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o5rz+72is%3c1)5q3!kidsserrn(-@!&f52c*$tmd(5mc6mdtz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
